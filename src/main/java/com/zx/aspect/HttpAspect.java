f831443ef410d519b16020610d9238b70d0fb3fd
BrightStarry <970389745@qq.com>
2017-04-03 16:34:37 +0800

    1

--- src/main/java/com/zx/aspect/HttpAspect.java
+++ src/main/java/com/zx/aspect/HttpAspect.java
@@ -1,8 +1,14 @@
 package com.zx.aspect;
 
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Created by 97038 on 2017-04-03.
@@ -10,9 +16,35 @@ import org.springframework.stereotype.Component;
 @Aspect
 @Component
 public class HttpAspect {
+    private final static Logger logger = LoggerFactory.getLogger(HttpAspect.class);
 
-    @Before("execution(public * com.zx.controller.GirlController.*(..))")
+    //切点
+    @Pointcut("execution(public * com.zx.controller.GirlController.*(..))")
     public void log(){
-        System.out.println("1111111");
+
+    }
+    @Before("log()")
+    public void doBefore(JoinPoint joinPoint){
+        ServletRequestAttributes attributes = (ServletRequestAttributes)RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+        //url
+        logger.info("url={}",request.getRequestURL());
+        //method
+        logger.info("method={}",request.getMethod());
+        //ip
+        logger.info("ip={}",request.getRemoteAddr());
+        //类方法
+        logger.info("class_method={}",joinPoint.getSignature().getDeclaringTypeName()+"."+joinPoint.getSignature().getName());
+        //参数
+        logger.info("args={}",joinPoint.getArgs());
+    }
+    @After("log()")
+    public void doAfter(){
+        logger.info("222222222222");
+    }
+
+    @AfterReturning(returning = "object",pointcut = "log()")
+    public void doAfterReturning(Object object){
+        logger.info("response={}",object.toString());
     }
 }
--- src/main/java/com/zx/pojo/Girl.java
+++ src/main/java/com/zx/pojo/Girl.java
@@ -1,11 +1,14 @@
 package com.zx.pojo;
 
 
+import org.hibernate.validator.constraints.NotBlank;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 
 /**
  * Created by 97038 on 2017-04-03.
@@ -15,8 +18,12 @@ public class Girl {
     @Id
     @GeneratedValue
     private Integer id;
+
+    @NotBlank(message = "这个字段必传")
     @Column(length = 4)
     private String cupSize;
+
+    @NotNull(message = "这个字段必传")
     @Min(value = 18,message = "未成年")
     private Integer age;
 
@@ -46,5 +53,14 @@ public class Girl {
 
 
     public Girl() {
+    }
+
+    @Override
+    public String toString() {
+        return "Girl{" +
+                "id=" + id +
+                ", cupSize='" + cupSize + '\'' +
+                ", age=" + age +
+                '}';
     }
 }
